8bd6bfe21f93d1f0b287b74a96135c50ed9190d1
Nguyen Hoa <hoathaigm@gmail.com>
2021-11-09 13:01:14 +0700

    study float css

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('float', { title: 'Express' });
 });
 
 router.get('/absolute', function(req, res, next) {
