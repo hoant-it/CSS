b8c244ffc5c38c60d24c918016fe99f7f076ce7a
Nguyen Hoa <hoathaigm@gmail.com>
2021-11-10 12:51:45 +0700

    study selector css

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('float', { title: 'Express' });
-});
-
-router.get('/absolute', function(req, res, next) {
-  res.render('absolute', { title: 'Express' });
+  res.render('specificity', { title: 'Express' });
 });
 
 
-router.get('/dropdown', function(req, res, next) {
-  res.render('dropdown', { title: 'Express' });
-});
 
 module.exports = router;
