183f423255d639ee1877da2a143862e6edf46fcc
Nguyen Hoa <hoathaigm@gmail.com>
2021-11-12 12:52:48 +0700

    transition css study

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('transform', { title: 'Express' });
+  res.render('transition', { title: 'Express' });
 });
 
 
