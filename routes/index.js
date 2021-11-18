96113fc0fb9413b253aa077e22e6eae3c77e8d8b
Nguyen Hoa <hoathaigm@gmail.com>
2021-11-18 19:56:49 +0700

    button tooltips css study

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('switchbutton', { title: 'Express' });
+  res.render('buttontooltips', { title: 'Express' });
 });
 
 
