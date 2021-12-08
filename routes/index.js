d51bc357fc810816e63e8a27a6a2985d0d21d2a9
Nguyen Hoa <hoathaigm@gmail.com>
2021-12-08 12:56:02 +0700

    touyubeclone build 20211208

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('gallelyImage', { title: 'Express' });
+  res.render('youtubeclone', { title: 'Express' });
 });
 
+router.get('/checklist', function(req, res, next) {
+  res.render('checklist', { title: 'Express' });
+});
 
 
 module.exports = router;
