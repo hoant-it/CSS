025a9c136d8cf9f35a97cae80b8c547ceba80e00
Nguyen Hoa <hoathaigm@gmail.com>
2021-12-11 13:02:48 +0700

    userdropdow css

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,14 @@ router.get('/checklist', function(req, res, next) {
   res.render('checklist', { title: 'Express' });
 });
 
+router.get('/dropdown', function(req, res, next) {
+  res.render('dropdown', { title: 'Express' });
+});
+
+router.get('/userdropdown', function(req, res, next) {
+  res.render('userdropdown', { title: 'Express' });
+});
+
+
 
 module.exports = router;
