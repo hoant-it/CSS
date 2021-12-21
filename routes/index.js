087f79894bec3255def593422c2a15a91ce3de5d
Nguyen Hoa <hoathaigm@gmail.com>
2021-12-21 10:26:45 +0700

    dropzone search youtube new code

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var multer= require('multer'); // for handling mutipart form data/images/files
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('youtubeclone', { title: 'Express' });
@@ -18,6 +18,63 @@ router.get('/userdropdown', function(req, res, next) {
   res.render('userdropdown', { title: 'Express' });
 });
 
+
+router.get('/searchyoutube', function(req, res, next) {
+  res.render('searchyoutube', { title: 'Express' });
+});
+
+
+router.get('/uploadprocessbar', function(req, res, next) {
+  res.render('uploadprocessbar', { title: 'Express' });
+});
+
+
+router.post('/uploadprocessbar', async (req, res) =>{
+
+  const storage = multer.diskStorage({
+    destination: function(req, res , cb){
+      cb(null,'./public/videos/')
+    },
+    filename: function(req,file, cb){
+      cb(null,file.originalname);
+    }
+  })
+  //for handling file size and name, we have to pass another argument i.e. 'storage' in the multer
+  // if you want limit the size, pass limit argument to the multer, limit take object
+  // 1 * 1024 * 1024= 1MB
+  //for handling file extension, pass 'filefillter' argument to the multer
+  
+  const upload = multer({
+    storage:storage,
+    // limits:{fileSize : 1 * 1024 * 1024 },
+    // fileFilter: function(req, file, cb){
+    //   if(file.mimetype=='video/mp4' 
+    //   // || file.mimetype == 'image/jpg' 
+    //   // || file.mimetype == 'image/jpeg'
+    //   ){
+    //     cb(null,true)
+    //   } else{
+    //     const err= new Error()
+    //     err.name='Extension Error'
+    //     err.message= 'Only video/mp4 files allowed'
+    //     return cb(err,false)
+    //   }
+
+    // }
+  
+  }).single('file');  // name og the input file
+
+
+  upload(req, res, (err) =>{
+    console.log(req.file);
+    if(err){
+      console.log(err);
+      return;
+    }
+    console.log('file uploaded')
+  })
+  
+})
 
 
 module.exports = router;
