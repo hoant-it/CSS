2efd45dd40e0056f3c56703cc922578c940b57bc
Nguyen Hoa <hoathaigm@gmail.com>
2021-12-23 14:00:33 +0700

    youtube v2 design 2:00 23-12-2021

--- public/javascripts/youtube/youtubeV2.js
+++ public/javascripts/youtube/youtubeV2.js
@@ -1,8 +1,28 @@
 var menuIcon= document.querySelector(".menu-icon");
 var sidebar= document.querySelector(".sidebar");
-// var menuIcon= document.querySelector(".menu-icon");
+var containerMain= document.querySelector(".container-main");
+
+
+function resize(){
+    if(window.innerWidth<700){
+        sidebar.classList.add("small-sidebar");
+        containerMain.classList.add("large-container-main")
+    }
+    else
+    {
+        sidebar.classList.remove("small-sidebar");
+        containerMain.classList.remove("large-container-main")
+    }
+
+}
+
+
 
 menuIcon.addEventListener("click" , function() {
     sidebar.classList.toggle("small-sidebar");
+    containerMain.classList.toggle("large-container-main")
 })
-
+window.addEventListener('resize', function(){
+    resize();
+})
+resize()
