5ddb148cdfcde1a75be71a2a702ca913cb6a5bbe
Nguyen Hoa <hoathaigm@gmail.com>
2022-01-08 08:40:21 +0700

    lucky number new code 8:40 08-01-2021

--- public/javascripts/luckynumber.js
+++ public/javascripts/luckynumber.js
@@ -5,9 +5,44 @@ var minNumber= 197005
 var maxNumber= 321787
 var balanceNumber= maxNumber-minNumber+1
 var timesRun=0;
-var arr=["may giat","tu lanh","tivi"]
+var divPt= document.getElementById("divPt");
+var arr=[ {STT:1, tenPT:"xe may vison", dv:"pcs", sl:1},
+{STT:2, tenPT:"tu lanh", dv:"pcs", sl:2},
+{STT:3, tenPT:"may giat", dv:"pcs", sl:4},
+]
 var numberFinal=0;
 var numberSpecial=221025
+
+
+
+function renderTablePhanThuong(){
+    if(arr.length>0){
+     var arrPtTable=    arr.map(item =>{
+         return `
+         <tr>
+             <td>${item.STT}</td>
+             <td>${item.tenPT}</td>
+             <td>${item.dv}</td>
+             <td>${item.sl}</td>
+         </tr>
+     `
+     });
+
+     var arrHtmlPt= arrPtTable.join('');
+     divPt.innerHTML=` <table  class="tbl-Pt">
+     <tr>
+         <th> STT </th>
+         <th> Phần Thường </th>
+         <th> Đơn Vị </th> 
+         <th> Số Lượng </th>
+     </tr>
+     ${arrHtmlPt}
+ </table>`
+
+    }
+}
+
+renderTablePhanThuong();
 
 btnStart.addEventListener("click", function () {
     if(arr.length<=0){
