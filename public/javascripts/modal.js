18c363a322de874b358fb0bc11e3a5d567b0511b
Nguyen Hoa <hoathaigm@gmail.com>
2022-01-17 15:44:51 +0700

    modal css new code 3:44 17-01-2022

---
+++ public/javascripts/modal.js
@@ -0,0 +1,23 @@
+
+function toggle() {
+    var blur= document.getElementById('blur');
+    blur.classList.toggle('active');
+    var popup= document.getElementById('popup');
+    popup.classList.toggle('active');
+
+}
+
+window.onload = function () {
+    var readMore= document.getElementById('readMore');
+    readMore.addEventListener('click',function () {
+        toggle();
+        
+    })
+
+    var close= document.getElementById('close');
+    close.addEventListener('click',function () {
+        toggle();
+        
+    })
+    
+}
